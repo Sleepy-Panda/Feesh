01f5383321b73ab77024ccbad6db952cf7e22beb
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2026-02-06 00:18:13 +0300

    Attach drop # to chat message

--- src/main/kotlin/com/github/sleepypanda/feesh/features/chat/RareDropMessage.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/features/chat/RareDropMessage.kt
@@ -5,37 +5,71 @@ import com.github.sleepypanda.feesh.events.models.RareDropEvent
 import com.github.sleepypanda.feesh.constants.RareDropTypes
 import com.github.sleepypanda.feesh.constants.RareDrops
 import com.github.sleepypanda.feesh.settings.categories.Chat
+import com.github.sleepypanda.feesh.settings.categories.Overlays
 import com.github.sleepypanda.feesh.utils.CommonUtils
 import com.github.sleepypanda.feesh.utils.ChatUtils
-import com.github.sleepypanda.feesh.utils.SoundUtils
-import com.github.sleepypanda.feesh.utils.RegisterUtils
-import com.github.sleepypanda.feesh.utils.PlayerUtils
+import com.github.sleepypanda.feesh.utils.data.PersistentDataManager
 import com.github.sleepypanda.feesh.utils.WorldUtils
-import com.github.sleepypanda.feesh.utils.enums.ColorCodes.*
-import com.github.sleepypanda.feesh.utils.enums.FormattingCodes.*
+import com.github.sleepypanda.feesh.FeeshMod
+
+// TODO It would be great to get drop number from fishing profit tracker instead of tracking it here,
+// But it requires a little exploration how to do it in a clean way.
 
 object RareDropMessage {
+    data class RareDropNotificationItem(
+        var count: Int = 0
+    )
+
+    data class RareDropNotificationsData(
+        val items: MutableMap<String, RareDropNotificationItem> = mutableMapOf()
+    )
+
     fun init() {
         EventBus.subscribe(RareDropEvent::class, ::onDrop)
     }
 
-    // TODO: Drop number #
-    // TODO: settings.includeDropNumberIntoDropMessage
+    fun reset() {
+        PersistentDataManager.feeshData.rareDropNotifications.items.clear()
+        saveData()
+    }
+
     private fun onDrop(event: RareDropEvent) {
-        if (!WorldUtils.isInSkyblock() || !Chat.messageOnRareDrops) return
+        try {
+            if (!WorldUtils.isInSkyblock() || !Chat.messageOnRareDrops) return
 
-        val itemName = event.itemName
-        val type = RareDropTypes.values().find { it.displayName == itemName } ?: return
-
-        if (!Chat.messageOnRareDropTypes.contains(type)) return
-
-        var metadata = listOf<String>()
-        if (event.magicFind != null && Chat.includeMagicFindIntoRareDropMessage) {
-            metadata += "+${event.magicFind} ✯ Magic Find"
+            val itemName = event.itemName
+            val type = RareDropTypes.values().find { it.displayName == itemName } ?: return
+    
+            if (!Chat.messageOnRareDropTypes.contains(type)) return
+    
+            var metadata = listOf<String>()
+            val dropNumber = getDropNumber(itemName)
+            if (dropNumber != null) {
+                metadata += "#$dropNumber"
+            }
+            if (event.magicFind != null && Chat.includeMagicFindIntoRareDropMessage) {
+                metadata += "+${event.magicFind} ✯ Magic Find"
+            }
+    
+            val message = getDropMessage(itemName, metadata)
+            ChatUtils.sendPartyChat(message) 
         }
+        catch (e: Exception) {
+            FeeshMod.LOGGER.error("[Feesh] Failed to send rare drop message.", e)
+        }
+    }
 
-        val message = getDropMessage(itemName, metadata)
-        ChatUtils.sendPartyChat(message)
+    private fun getDropNumber(itemName: String): Int? {
+        if (!Chat.includeDropNumberIntoDropMessage || !Overlays.fishingProfitTrackerOverlay) return null
+
+        val dropInfo = RareDrops.rareDrops.find { it.itemName == itemName } ?: return null
+        val items = PersistentDataManager.feeshData.rareDropNotifications.items
+        val currentCount = items[dropInfo.id]?.count ?: 0
+        val newCount = currentCount + 1
+        items[dropInfo.id] = RareDropNotificationItem(newCount)
+        saveData()
+
+        return newCount
     }
 
     private fun getDropMessage(itemName: String, metadata: List<String>): String {
@@ -43,5 +77,9 @@ object RareDropMessage {
         val metadataString = if (metadata.isNotEmpty()) " (${metadata.joinToString(", ")})" else ""
 
         return "--> ${article} ${itemName} has dropped${metadataString} <--"
+    }
+
+    private fun saveData() {
+        PersistentDataManager.saveFeeshDataToFileAsync()
     }
 }
