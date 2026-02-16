42f15d4ce275662133da6c9a308bfb570b6ee08f
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2026-02-16 18:32:38 +0300

    Fixed the mixin

--- src/main/java/com/github/sleepypanda/feesh/mixin/EntityRendererMixin.java
+++ src/main/java/com/github/sleepypanda/feesh/mixin/EntityRendererMixin.java
@@ -1,10 +1,13 @@
 package com.github.sleepypanda.feesh.mixin;
 
 import com.github.sleepypanda.feesh.features.overlays.FishingHookTimer;
+import com.github.sleepypanda.feesh.features.rendering.HideOtherPlayersHooks;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,8 +17,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class EntityRendererMixin<T extends Entity> {
     @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
     private void feesh$onShouldRender(T entity, Frustum frustum, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
-        if (entity instanceof ArmorStandEntity armorStand && FishingHookTimer.INSTANCE.shouldCancelArmorStandRendering(armorStand.getUuid())) {
+        if (entity instanceof ArmorStandEntity armorStand && FishingHookTimer.shouldCancelArmorStandRendering(armorStand.getUuid())) {
             cir.setReturnValue(false);
+        }
+        if (entity instanceof FishingBobberEntity fishingBobber && HideOtherPlayersHooks.shouldHideOtherPlayersHooks()) {
+            var player = MinecraftClient.getInstance().player;
+            if (player != null && fishingBobber.getOwner() != player) {
+                cir.setReturnValue(false);
+            }
         }
     }
 }
--- src/main/kotlin/com/github/sleepypanda/feesh/features/rendering/HideOtherPlayersHooks.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/features/rendering/HideOtherPlayersHooks.kt
@@ -4,6 +4,7 @@ import com.github.sleepypanda.feesh.settings.categories.Rendering
 import com.github.sleepypanda.feesh.utils.WorldUtils
 
 object HideOtherPlayersHooks {
+    @JvmStatic
     fun shouldHideOtherPlayersHooks(): Boolean {
         if (!WorldUtils.isInSkyblock() || !WorldUtils.isInFishingWorld()) return false
         if (!Rendering.hideOtherPlayersFishingHooks) return false
