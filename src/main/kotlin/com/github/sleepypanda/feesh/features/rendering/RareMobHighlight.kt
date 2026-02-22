8707bbe2b2748c4574bb075dc6724ea84bb6e20f
dsgameover <211853934+dsgameover@users.noreply.github.com>
2026-02-22 21:40:49 +0200

    made eventbus in general, also removed thread
    i started using kotlin way of delay, also made sure that it wont throw exception if mob dies in 500ms after spawn via 
    net.minecraft.client.MinecraftClient.getInstance().execute {

--- src/main/kotlin/com/github/sleepypanda/feesh/features/rendering/RareMobHighlight.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/features/rendering/RareMobHighlight.kt
@@ -8,26 +8,37 @@ import com.github.sleepypanda.feesh.settings.categories.WorldRendering
 import com.github.sleepypanda.feesh.utils.ChatUtils
 import com.github.sleepypanda.feesh.utils.ChatUtils.removeFormatting
 import com.github.sleepypanda.feesh.utils.WorldUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.minecraft.block.entity.VaultBlockEntity
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.decoration.ArmorStandEntity
 import net.minecraft.world.World
+import net.minecraft.world.tick.Tick
+import net.minecraft.world.tick.TickScheduler
+import java.awt.EventQueue
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 object RareMobHighlight {
     @JvmField
     val highlightedEntities = mutableMapOf<Int, Int>()
-
+    private val modScope = CoroutineScope(Dispatchers.Default)
     fun init() {
+        EventBus.subscribe(WorldChangedEvent::class, ::worldChange)
+
         ClientEntityEvents.ENTITY_LOAD.register { entity, _ ->
             if (!WorldRendering.highlightSeaCreatures || !WorldUtils.isInFishingWorld()) return@register
-
-            if (entity is LivingEntity) {
-                Thread {
-                    Thread.sleep(500)
+            modScope.launch {
+                delay(500)
+                net.minecraft.client.MinecraftClient.getInstance().execute {
+                    if (entity !is LivingEntity || !entity.isAlive) return@execute
                     val plainName = entity.customName?.string?.removeFormatting()
-
                     if (plainName?.contains("[Lv") == true && plainName.contains("❤")) {
                         val info = SeaCreatures.allSeaCreatures.find { creatureInfo ->
                             val cleanDbName = creatureInfo.name.removeFormatting()
@@ -35,28 +46,25 @@ object RareMobHighlight {
                         }
 
                         if (info != null && info.isRare) {
-                            net.minecraft.client.MinecraftClient.getInstance().execute {
-                                val mobEntity = entity.entityWorld.getEntityById(entity.id - 1) as? LivingEntity
-                                    ?: entity.entityWorld.getOtherEntities(
-                                        entity,
-                                        entity.boundingBox.expand(1.0, 2.0, 1.0)
-                                    ) {
-                                        it is LivingEntity && it !is ArmorStandEntity
-                                    }.firstOrNull() as? LivingEntity
+                            val mobEntity = entity.entityWorld.getEntityById(entity.id - 1) as? LivingEntity
+                                ?: entity.entityWorld.getOtherEntities(
+                                    entity,
+                                    entity.boundingBox.expand(1.0, 2.0, 1.0)
+                                ) {
+                                    it is LivingEntity && it !is ArmorStandEntity
+                                }.firstOrNull() as? LivingEntity
 
-                                if (mobEntity != null) {
-                                    applyGlow(mobEntity, info.name)
-                                }
+                            if (mobEntity != null) {
+                                applyGlow(mobEntity, info.name)
                             }
                         }
                     }
-                }.start()
+                }
             }
         }
-        if (highlightedEntities.isNotEmpty()) EventBus.subscribe(WorldChangedEvent::class, ::worldChange)
 
         ClientTickEvents.END_CLIENT_TICK.register { client ->
-            if(!WorldUtils.isInFishingWorld()) return@register
+            if (!WorldUtils.isInFishingWorld()) return@register
             val world = client.world ?: return@register
             if (!WorldRendering.highlightSeaCreatures) {
                 if (highlightedEntities.isNotEmpty()) {
@@ -83,6 +91,6 @@ object RareMobHighlight {
     }
 
     private fun worldChange(event: WorldChangedEvent) {
-        highlightedEntities.clear()
+        if (highlightedEntities.isNotEmpty()) highlightedEntities.clear()
     }
 }
