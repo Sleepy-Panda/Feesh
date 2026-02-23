f124ff84515ad15566b7ebf494bcf54b38fabcb0
dsgameover <211853934+dsgameover@users.noreply.github.com>
2026-02-24 01:03:03 +0200

    removed highlightstack function, and made it so when titanoboa armorstand is found it doesnt run through the entire carriers
    if you could test this it would be fire.

--- src/main/kotlin/com/github/sleepypanda/feesh/features/rendering/RareMobHighlight.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/features/rendering/RareMobHighlight.kt
@@ -2,8 +2,8 @@ package com.github.sleepypanda.feesh.features.rendering
 
 import com.github.sleepypanda.feesh.constants.SeaCreatures
 import com.github.sleepypanda.feesh.events.EventBus
-import com.github.sleepypanda.feesh.events.models.ArmorStandLoadedEvent
 import com.github.sleepypanda.feesh.events.models.ClientTickEvent
+import com.github.sleepypanda.feesh.events.models.ArmorStandLoadedEvent
 import com.github.sleepypanda.feesh.events.models.WorldChangedEvent
 import com.github.sleepypanda.feesh.settings.categories.WorldRendering
 import com.github.sleepypanda.feesh.utils.ChatUtils.removeFormatting
@@ -12,6 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import net.minecraft.client.MinecraftClient
 import kotlin.jvm.JvmField
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.decoration.ArmorStandEntity
@@ -33,7 +34,7 @@ object RareMobHighlight {
         val entity = event.entity
         modScope.launch {
             delay(500)
-            net.minecraft.client.MinecraftClient.getInstance().execute {
+            MinecraftClient.getInstance().execute {
                 val plainName = entity.customName?.string?.removeFormatting()
                 if (plainName?.contains("[Lv") == true && plainName.contains("❤")) {
                     val info = SeaCreatures.allSeaCreatures.find { creatureInfo ->
@@ -44,24 +45,29 @@ object RareMobHighlight {
                     if (info != null && (info.name.contains("Jawbus") || info.name.contains("Wiki Tiki") || info.isRare)) {
                         //finds the head of all entities.
                         var root: net.minecraft.entity.Entity = entity
-                        while (root.vehicle != null) {
-                            root = root.vehicle!!
+                        if (info.name.contains("Titanoboa")) root = root.vehicle!!
+                        else {
+                            while (root.vehicle != null) {
+                                root = root.vehicle!!
+                            }
                         }
 
                         // Identify if the stack actually contains a mob (besides the armorStand)
                         val hasMobInStack = hasLivingPassenger(root)
 
                         if (hasMobInStack) {
-                            highlightFullStack(root, info.name)
-                        } else {
-                            // mob searcher since it didn't find a mob attached to the armorStand
-                            val nearbyMob =
-                                entity.entityWorld.getOtherEntities(entity, entity.boundingBox.expand(1.0)) {
-                                    it is LivingEntity && it !is ArmorStandEntity
-                                }.firstOrNull() as? LivingEntity
+                            if (root is LivingEntity && root !is ArmorStandEntity && root !is PlayerEntity) {
+                                applyGlow(root, info.name)
+                            } else {
+                                // mob searcher since it didn't find a mob attached to the armorStand
+                                val nearbyMob =
+                                    entity.entityWorld.getOtherEntities(entity, entity.boundingBox.expand(1.0)) {
+                                        it is LivingEntity && it !is ArmorStandEntity
+                                    }.firstOrNull() as? LivingEntity
 
-                            if (nearbyMob != null) {
-                                applyGlow(nearbyMob, info.name)
+                                if (nearbyMob != null) {
+                                    applyGlow(nearbyMob, info.name)
+                                }
                             }
                         }
                     }
@@ -93,21 +99,14 @@ object RareMobHighlight {
     }
 
     private fun applyGlow(target: LivingEntity, cleanName: String) {
-        if(cleanName.contains("Follower") || cleanName.contains("Laser Totem"))
-            highlightedEntities[target.id] = 0xFF0000
-        else
-            highlightedEntities[target.id] = 0x00FFFF
+        if (cleanName.contains("Follower") || cleanName.contains("Laser Totem")) highlightedEntities[target.id] =
+            0xFF0000
+        else highlightedEntities[target.id] = 0x00FFFF
         target.isGlowing = true
     }
 
     private fun worldChange(event: WorldChangedEvent) {
         if (highlightedEntities.isNotEmpty()) highlightedEntities.clear()
-    }
-
-    private fun highlightFullStack(base: net.minecraft.entity.Entity, name: String) {
-        if (base is LivingEntity && base !is ArmorStandEntity && base !is PlayerEntity) {
-            applyGlow(base, name)
-        }
     }
 
     private fun hasLivingPassenger(base: net.minecraft.entity.Entity): Boolean {
