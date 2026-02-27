69fe9e0c09fbd0d2ea0f783596484bcee8da284f
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2026-02-28 02:47:37 +0300

    More fixes x2

--- src/main/kotlin/com/github/sleepypanda/feesh/features/rendering/RareMobHighlight.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/features/rendering/RareMobHighlight.kt
@@ -17,21 +17,27 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.jvm.JvmField
-import net.minecraft.entity.mob.MobEntity
 import net.minecraft.entity.decoration.ArmorStandEntity
-import net.minecraft.entity.mob.GuardianEntity
+import net.minecraft.entity.mob.MagmaCubeEntity
 import net.minecraft.client.MinecraftClient
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.mob.SlimeEntity
 
 // TODO Fire Eel, other debug creatures to remove
-// TODO Do not parse all nametags after world swap
+// TODO Do not parse nametags after world swap
 // TODO Observable setting to cleanup on disable
+// TODO: Slugs and Pyro not highlighted when they are followers, highlighted when normal mobs
+// TODO: Highlight full wiki tiki
+
+
 object RareMobHighlight {
     @JvmField
     val highlightedEntities = mutableMapOf<Int, Int>()
     private val modScope = CoroutineScope(Dispatchers.Default)
+
+    private val debugEntities = listOf("Baby Magma Slug", "Magma Slug", "Moogma", "Taurus", "Lava Leech", "Pyroclastic Worm", "Lava Flame", "Fire Eel");
+    private val extraEntities = listOf("Wiki Tiki Laser Totem", "Jawbus Follower");
 
     fun init() {
         EventBus.subscribe(WorldChangedEvent::class, ::worldChange)
@@ -46,26 +52,11 @@ object RareMobHighlight {
         modScope.launch {
             delay(250)
             MinecraftClient.getInstance().execute {
-                // TODO: Pass parameter for allowed sc names to not parse all nametags?
-                val cleanName = EntityUtils.parseSeaCreatureNametag(entity)?.baseMobName ?: return@execute
-                val info = SeaCreatures.rareSeaCreatures.find { it.name == cleanName }
+                val mobNames = SeaCreatures.rareSeaCreatures.map { it.name } + debugEntities + extraEntities
+                val cleanName = EntityUtils.parseSeaCreatureNametag(entity, mobNames)?.baseMobName ?: return@execute
+                val rareScInfo = SeaCreatures.rareSeaCreatures.find { it.name == cleanName }
 
-                // Titanoboa
-                //[21:04:00] [Render thread/INFO]: Mob entity by ID: 102114 translation{key='entity.minecraft.slime', args=[]}
-                //[21:04:00] [Render thread/INFO]: Vehicle: null null
-                //[21:04:00] [Render thread/INFO]: Passengers: null null null
-
-                // Alligator: Mob entity by ID: 164758 translation{key='entity.minecraft.player', args=[]}
-
-                // Eel: tail highlighted
-                //[23:11:04] [Render thread/INFO]: Mob entity by ID: 281131 translation{key='entity.minecraft.slime', args=[]}
-                //[23:11:04] [Render thread/INFO]: Vehicle: null null
-                //[23:11:04] [Render thread/INFO]: Passengers: null null null
-
-                val debugEntities = listOf("Baby Magma Slug", "Magma Slug", "Moogma", "Taurus", "Lava Leech", "Pyroclastic Worm", "Lava Flame", "Fire Eel");
-                val extraEntities = listOf("Wiki Tiki Laser Totem", "Jawbus Follower");
-
-                if (info != null || cleanName in extraEntities || cleanName in debugEntities) {
+                if (rareScInfo != null || cleanName in extraEntities || cleanName in debugEntities) {
                     FeeshMod.LOGGER.info(cleanName + " found / ${entity.customName?.string}")
                     val mobEntity = entity.entityWorld.getEntityById(entity.id - 1) as? LivingEntity
                     FeeshMod.LOGGER.info("Mob entity by ID: ${mobEntity?.id} ${mobEntity?.type?.name} ${mobEntity?.isAlive}")
@@ -97,7 +88,7 @@ object RareMobHighlight {
                     }
                 }
 
-                if (info == null && cleanName !in extraEntities && cleanName !in debugEntities) return@execute
+                if (rareScInfo == null && cleanName !in extraEntities && cleanName !in debugEntities) return@execute
 
                 if (cleanName.contains("Jawbus Follower")) FeeshMod.LOGGER.info("Follower checks started")
 
@@ -108,28 +99,24 @@ object RareMobHighlight {
                 }
 
                 var mobEntity = entity.entityWorld.getEntityById(entity.id - shift) as? LivingEntity ?: return@execute
-                if (cleanName.contains("Jawbus Follower") && mobEntity is SlimeEntity) { // Fire Eel
-                    mobEntity = entity.entityWorld.getEntityById(entity.id - 11) as? LivingEntity ?: return@execute
+                if (cleanName.contains("Jawbus Follower") && mobEntity is SlimeEntity && mobEntity !is MagmaCubeEntity) { // Fire Eel
+                    mobEntity = entity.entityWorld.getEntityById(entity.id - 11) as? LivingEntity ?: return@execute // Find its head
                 }
 
                 if (!mobEntity.isAlive) return@execute
                 if (mobEntity is PlayerEntity && (mobEntity.uuid.version() == 4 || mobEntity.uuid.version() == 1)) return@execute
 
-
-
                 if (cleanName.contains("Jawbus Follower")) FeeshMod.LOGGER.info("Follower still here")
-                // TODO: Add rider for ragnarok
-                // TODO: Highlight full wiki tiki
        
                 val color = when {
-                    info?.rarityColorCode == ColorCodes.COMMON.code -> 0xFFFFFF
-                    info?.rarityColorCode == ColorCodes.UNCOMMON.code -> 0x55FF55
-                    info?.rarityColorCode == ColorCodes.RARE.code -> 0x8AA1FF
-                    info?.rarityColorCode == ColorCodes.EPIC.code -> 0xAA00AA
-                    info?.rarityColorCode == ColorCodes.LEGENDARY.code -> 0xFFAA00
-                    info?.rarityColorCode == ColorCodes.MYTHIC.code -> 0xFF55FF
-                    info?.rarityColorCode == ColorCodes.DIVINE.code -> 0x55FFFF
-                    info?.rarityColorCode == ColorCodes.SPECIAL.code -> 0xFF5555
+                    rareScInfo?.rarityColorCode == ColorCodes.COMMON.code -> 0xFFFFFF
+                    rareScInfo?.rarityColorCode == ColorCodes.UNCOMMON.code -> 0x55FF55
+                    rareScInfo?.rarityColorCode == ColorCodes.RARE.code -> 0x8AA1FF
+                    rareScInfo?.rarityColorCode == ColorCodes.EPIC.code -> 0xAA00AA
+                    rareScInfo?.rarityColorCode == ColorCodes.LEGENDARY.code -> 0xFFAA00
+                    rareScInfo?.rarityColorCode == ColorCodes.MYTHIC.code -> 0xFF55FF
+                    rareScInfo?.rarityColorCode == ColorCodes.DIVINE.code -> 0x55FFFF
+                    rareScInfo?.rarityColorCode == ColorCodes.SPECIAL.code -> 0xFF5555
                     cleanName in extraEntities -> 0xF01616
                     cleanName in debugEntities -> 0xF01616 // TODO Remove
                     else -> 0x00FFFF
@@ -137,8 +124,12 @@ object RareMobHighlight {
 
                 applyGlow(mobEntity, color)
 
-                if (mobEntity.vehicle != null && mobEntity.vehicle is LivingEntity) {
+                if (mobEntity.vehicle != null && mobEntity.vehicle is LivingEntity) { // The Loch Emperor's guardian, etc
                     applyGlow(mobEntity.vehicle as LivingEntity, color)
+                }
+
+                if (mobEntity.firstPassenger != null && mobEntity.firstPassenger is LivingEntity) { // Ragnarok's rider
+                    applyGlow(mobEntity.firstPassenger as LivingEntity, color)
                 }
             }
         }
