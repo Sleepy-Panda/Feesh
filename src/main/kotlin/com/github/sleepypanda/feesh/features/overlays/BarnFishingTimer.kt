bd07accc2dfbd3f3e770dbe3f5db4f5be3adac1f
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2026-05-19 20:46:28 +0300

    Removed imports

--- src/main/kotlin/com/github/sleepypanda/feesh/features/overlays/BarnFishingTimer.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/features/overlays/BarnFishingTimer.kt
@@ -9,8 +9,6 @@ import com.github.sleepypanda.feesh.settings.categories.Overlays
 import com.github.sleepypanda.feesh.settings.categories.Alerts
 import com.github.sleepypanda.feesh.utils.WorldUtils
 import com.github.sleepypanda.feesh.utils.PlayerUtils
-import com.github.sleepypanda.feesh.utils.EntityUtils
-import com.github.sleepypanda.feesh.utils.ChatUtils
 import com.github.sleepypanda.feesh.utils.ChatUtils.getFormattedString
 import com.github.sleepypanda.feesh.utils.ChatUtils.removeFormatting
 import com.github.sleepypanda.feesh.utils.gui.FeeshGui
@@ -20,13 +18,11 @@ import com.github.sleepypanda.feesh.utils.CommonUtils
 import com.github.sleepypanda.feesh.utils.SoundUtils
 import com.github.sleepypanda.feesh.utils.RegisterUtils
 import com.github.sleepypanda.feesh.utils.enums.ColorCodes.*
-import com.github.sleepypanda.feesh.utils.enums.FormattingCodes.*
 import com.github.sleepypanda.feesh.constants.SeaCreatures
 import com.github.sleepypanda.feesh.constants.Sounds
 import com.github.sleepypanda.feesh.settings.categories.General
 import com.github.sleepypanda.feesh.settings.categories.SoundMode
 import net.minecraft.world.entity.decoration.ArmorStand
-import net.minecraft.world.entity.EquipmentSlot
 import java.util.Date
 
 object BarnFishingTimer {
