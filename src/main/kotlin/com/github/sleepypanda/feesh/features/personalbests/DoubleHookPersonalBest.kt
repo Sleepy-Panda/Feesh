146bdbba99b28ba1d99d3131fb10271dbc079f43
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2026-06-02 02:16:05 +0300

    Show previous PB

--- src/main/kotlin/com/github/sleepypanda/feesh/features/personalbests/DoubleHookPersonalBest.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/features/personalbests/DoubleHookPersonalBest.kt
@@ -4,8 +4,6 @@ import com.github.sleepypanda.feesh.constants.SeaCreatureNames
 import com.github.sleepypanda.feesh.events.EventBus
 import com.github.sleepypanda.feesh.events.models.OwnSeaCreatureCaughtEvent
 import com.github.sleepypanda.feesh.events.models.WorldChangedEvent
-import com.github.sleepypanda.feesh.settings.categories.General
-import com.github.sleepypanda.feesh.settings.categories.SoundMode
 import com.github.sleepypanda.feesh.utils.ChatUtils
 import com.github.sleepypanda.feesh.utils.SoundUtils
 import com.github.sleepypanda.feesh.utils.WorldUtils
@@ -43,17 +41,18 @@ object DoubleHookPersonalBest {
 
     private fun checkAndAnnouncePersonalBest(currentStreak: Int) {
         CommonUtils.runWithCatching("Failed to check and announce double hook PB") {
-            if (currentStreak == 0) return
+            if (currentStreak <= 1) return
 
             val personalBestEntry = PersistentDataManager.feeshData.personalBest.doubleHookStreak
-            if (currentStreak <= personalBestEntry.amount) return
+            val previousBest = personalBestEntry.amount
+            if (currentStreak <= previousBest) return
     
             personalBestEntry.amount = currentStreak
             personalBestEntry.at = Date()
             PersistentDataManager.saveFeeshDataToFileAsync()
     
             ChatUtils.sendLocalChat(
-                "${LIGHT_PURPLE}${BOLD}PERSONAL BEST!${RESET} Double Hook streak: ${GREEN}${BOLD}$currentStreak",
+                "${LIGHT_PURPLE}${BOLD}PERSONAL BEST!${RESET} Double Hook streak: ${WHITE}${BOLD}${previousBest} ${GRAY}-> ${GREEN}${BOLD}$currentStreak",
                 true
             )
             CommonUtils.showTitle("${LIGHT_PURPLE}${BOLD}PERSONAL BEST!", "DH streak: ${GREEN}$currentStreak")
