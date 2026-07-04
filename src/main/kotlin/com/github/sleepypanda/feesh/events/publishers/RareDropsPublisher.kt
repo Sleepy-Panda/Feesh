31761afc0dcf0bf25f1ae3b84176edd2f1166719
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2026-07-04 16:23:38 +0300

    Fixed functionalities being broken in the mod due to Hypixel changing various text symbols.

--- src/main/kotlin/com/github/sleepypanda/feesh/events/publishers/RareDropsPublisher.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/events/publishers/RareDropsPublisher.kt
@@ -12,11 +12,14 @@ import com.github.sleepypanda.feesh.utils.CommonUtils
 object RareDropsPublisher {
     // §6§lRARE DROP! §dRadioactive Vial §b(+§b236 §b✯ Magic Find§b)
     // §6§lRARE DROP! §6Tiki Mask §b(+§b236 §b✯ Magic Find§b)
-    val RARE_DROP_PATTERN = Regex("^§6§lRARE DROP! (?<item>.+?) §b\\(\\+§b(?<mf>\\d+) §b✯ Magic Find§b\\)$")
+    //  can be used instead of ✯ as the Magic Find symbol:
+    // §6§lRARE DROP! §dRadioactive Vial §b(+§b236 §b Magic Find§b)
+    val RARE_DROP_PATTERN = Regex("^§6§lRARE DROP! (?<item>.+?)( §b\\(\\+§b(?<mf>\\d+) §b. Magic Find§b\\))?$")
 
     // §6§lRARE DROP! §fEnchanted Book (Charm 1§f) §b(+§b293 §b✯ Magic Find§b)
     // §6§lRARE DROP! §fEnchanted Book (§d§lFlash 1§f) §b(+§b392 §b✯ Magic Find§b)
-    val RARE_DROP_BOOK_PATTERN = Regex("^§6§lRARE DROP! §fEnchanted Book \\((?<bookName>.+?)§f\\) §b\\(\\+§b(?<mf>\\d+) §b✯ Magic Find§b\\)$")
+    // §6§lRARE DROP! §fEnchanted Book (§d§lFlash I§f) §b(+§b392 §b✯ Magic Find§b)
+    val RARE_DROP_BOOK_PATTERN = Regex("^§6§lRARE DROP! §fEnchanted Book \\((?<bookName>.+?)§f\\)( §b\\(\\+§b(?<mf>\\d+) §b. Magic Find§b\\))?$")
 
     // §6§lPET DROP! §6§lLEGENDARY §6Baby Yeti (SH format)
     // §6§lPET DROP! §6Baby Yeti
@@ -37,7 +40,7 @@ object RareDropsPublisher {
     // §6⛃ §r§6§lGREAT CATCH! §r§fYou caught a §r§7[Lvl 1] §f§lCOMMON §fSquid§r§f!
     // §6⛃ §r§6§lGREAT CATCH! §r§fYou caught a §r§7[Lvl 1] §a§lUNCOMMON §aSquid§r§f!
     // §d⛃ §r§d§lOUTSTANDING CATCH! §r§fYou caught a §r§7[Lvl 1] §6§lLEGENDARY §6Squid§r§f!
-    val PET_CATCH_PATTERN = Regex("^(§6⛃ (§r)?§6§lGREAT CATCH!|§d⛃ (§r)?§d§lOUTSTANDING CATCH!) (§r)?§fYou caught a (§r)?(§r)?§7\\[Lvl 1\\] (?<shRarity>(§6§lLEGENDARY |§5§lEPIC |§9§lRARE |§a§lUNCOMMON |§f§lCOMMON ))?+(?<pet>.+?)§f!$")
+    val PET_CATCH_PATTERN = Regex("^(§6. (§r)?§6§lGREAT CATCH!|§d. (§r)?§d§lOUTSTANDING CATCH!) (§r)?§fYou caught a (§r)?(§r)?§7\\[Lvl 1] (?<shRarity>(§6§lLEGENDARY |§5§lEPIC |§9§lRARE |§a§lUNCOMMON |§f§lCOMMON ))?+(?<pet>.+?)§f!$")
 
     fun init() {
         EventBus.subscribe(ChatCancellableEvent::class, ::onChat)
@@ -49,11 +52,11 @@ object RareDropsPublisher {
         CommonUtils.runWithCatching("Failed to handle rare drop in publisher.") {
             val playerName = PlayerUtils.getUnformattedName()
             if (playerName.isNullOrEmpty()) return@onChat
-            
+
             val rareDropBookMatch = RARE_DROP_BOOK_PATTERN.matchEntire(event.formattedText)
             if (rareDropBookMatch != null) {
                 val item = rareDropBookMatch.groups.get("bookName")?.value ?: return@onChat
-                val magicFind = rareDropBookMatch.groups.get("mf")?.value?.toIntOrNull()
+                val magicFind = rareDropBookMatch.groups.get("mf")?.value?.removeFormatting()?.toIntOrNull()
                 EventBus.publish(RareDropEvent(item.removeFormatting(), item, magicFind))
                 return@onChat
             }
@@ -61,7 +64,7 @@ object RareDropsPublisher {
             val rareDropMatch = RARE_DROP_PATTERN.matchEntire(event.formattedText)
             if (rareDropMatch != null) {
                 val item = rareDropMatch.groups.get("item")?.value ?: return@onChat
-                val magicFind = rareDropMatch.groups.get("mf")?.value?.toIntOrNull()
+                val magicFind = rareDropMatch.groups.get("mf")?.value?.removeFormatting()?.toIntOrNull()
                 EventBus.publish(RareDropEvent(item.removeFormatting(), item, magicFind))
                 return@onChat
             }
