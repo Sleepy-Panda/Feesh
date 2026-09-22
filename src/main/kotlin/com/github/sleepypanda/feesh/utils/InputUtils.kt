47f2e06f1be0422e11cf0d501cb3dbd7f7e79300
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2026-09-22 18:58:08 +0300

    Migrated to InputConstants instead of GLFW where possible

--- src/main/kotlin/com/github/sleepypanda/feesh/utils/InputUtils.kt
+++ src/main/kotlin/com/github/sleepypanda/feesh/utils/InputUtils.kt
@@ -2,28 +2,11 @@ package com.github.sleepypanda.feesh.utils
 
 import com.mojang.blaze3d.platform.InputConstants
 import net.minecraft.client.input.InputWithModifiers
-//#if MC >= 26.3
-//$$ // GLFW was replaced by SDL in 26.3; use InputConstants instead.
-//#else
-import org.lwjgl.glfw.GLFW
-//#endif
 
 object InputUtils {
-    fun unboundKey(): Int {
-        //#if MC >= 26.3
-        //$$ return InputConstants.UNKNOWN.getValue()
-        //#else
-        return GLFW.GLFW_KEY_UNKNOWN
-        //#endif
-    }
+    fun unboundKey(): Int = InputConstants.UNKNOWN.getValue()
 
-    fun pauseKey(): Int {
-        //#if MC >= 26.3
-        //$$ return InputConstants.KEY_PAUSE
-        //#else
-        return GLFW.GLFW_KEY_PAUSE
-        //#endif
-    }
+    fun pauseKey(): Int = InputConstants.KEY_PAUSE
 
     fun keyType(): InputConstants.Type {
         //#if MC >= 26.3
@@ -39,43 +22,15 @@ object InputUtils {
 
     fun hasControlDown(input: InputWithModifiers?): Boolean = input?.let { hasControlDown(it.modifiers()) } ?: false
 
-    fun isLeftMouseButton(button: Int): Boolean {
-        //#if MC >= 26.3
-        //$$ return button == InputConstants.MOUSE_BUTTON_LEFT
-        //#else
-        return button == 0
-        //#endif
-    }
+    fun isLeftMouseButton(button: Int): Boolean = button == InputConstants.MOUSE_BUTTON_LEFT
 
-    fun isEscapeKey(keyCode: Int): Boolean {
-        //#if MC >= 26.3
-        //$$ return keyCode == InputConstants.KEY_ESCAPE
-        //#else
-        return keyCode == GLFW.GLFW_KEY_ESCAPE
-        //#endif
-    }
+    fun isEscapeKey(keyCode: Int): Boolean = keyCode == InputConstants.KEY_ESCAPE
 
     fun isPlusKey(keyCode: Int): Boolean {
-        //#if MC >= 26.3
-        //$$ return keyCode == InputConstants.KEY_EQUALS || keyCode == InputConstants.KEY_ADD
-        //#else
-        return keyCode == GLFW.GLFW_KEY_EQUAL || keyCode == GLFW.GLFW_KEY_KP_ADD
-        //#endif
+        return keyCode == InputConstants.KEY_EQUALS || keyCode == InputConstants.KEY_ADD
     }
 
-    fun isMinusKey(keyCode: Int): Boolean {
-        //#if MC >= 26.3
-        //$$ return keyCode == InputConstants.KEY_MINUS
-        //#else
-        return keyCode == GLFW.GLFW_KEY_MINUS || keyCode == GLFW.GLFW_KEY_KP_SUBTRACT
-        //#endif
-    }
+    fun isMinusKey(keyCode: Int): Boolean = keyCode == InputConstants.KEY_MINUS
 
-    fun isZeroKey(keyCode: Int): Boolean {
-        //#if MC >= 26.3
-        //$$ return keyCode == InputConstants.KEY_0
-        //#else
-        return keyCode == GLFW.GLFW_KEY_0
-        //#endif
-    }
+    fun isZeroKey(keyCode: Int): Boolean = keyCode == InputConstants.KEY_0
 }
